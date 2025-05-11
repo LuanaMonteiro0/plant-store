b8f232170d8275739fd67e23157519c29b742c47
Luan1611 <luan_1611@hotmail.com>
2025-05-11 12:09:33 -0300

    delete-plant function updated

--- apps/plant-store/src/modules/plant/use-cases/delete-plant/task.ts
+++ apps/plant-store/src/modules/plant/use-cases/delete-plant/task.ts
@@ -1,3 +1,27 @@
-export function deletePlant() {
+import { TunnelCatClient } from '@core/tunnelCat'
+import { DeletePlantDTO } from './dto'
 
+interface deletePlantParams extends DeletePlantDTO {}
+
+export async function deletePlant(
+  client: TunnelCatClient,
+  params: deletePlantParams,
+) {
+  try {
+    client.startTransaction()
+
+    const query = `
+    DELETE FROM plant WHERE id=$id
+    `
+    const values = {
+      id: params.id,
+    }
+
+    await client.query({ query, values })
+
+    client.commitTransaction()
+  } catch (err) {
+    console.log(err)
+    client.rollbackTransaction()
+  }
 }
