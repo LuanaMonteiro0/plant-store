c78298f1334925401e4677aecc9a2b62a7cd0e03
lumonteiro0 <luxo11111@gmail.com>
2025-05-11 11:33:37 -0300

    feat: dto added in plant-type

--- apps/plant-store/src/modules/plant/use-cases/create-plant/dto.ts
+++ apps/plant-store/src/modules/plant/use-cases/create-plant/dto.ts
@@ -1,3 +1,56 @@
+import {
+  IsInt,
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsBoolean,
+  IsNumber,
+  IsDate,
+  IsPositive,
+  Min,
+  Max,
+} from 'class-validator'
+import { Type } from 'class-transformer'
+
 export class createPlantDTO {
-    
+  @IsString()
+  @IsNotEmpty()
+  name: string
+
+  @IsString()
+  @IsNotEmpty()
+  subtitle: string
+
+  @IsNumber()
+  @IsPositive()
+  price: number
+
+  @IsOptional()
+  @IsInt()
+  @Min(0)
+  @Max(100)
+  discount_percentage?: number
+
+  @IsString()
+  @IsNotEmpty()
+  description: string
+
+  @IsString()
+  @IsNotEmpty()
+  features: string
+
+  @IsOptional()
+  @IsString()
+  img_url?: string
+
+  @IsBoolean()
+  is_in_sale: boolean
+
+  @Type(() => Date)
+  @IsDate()
+  created_at: Date
+
+  @Type(() => Date)
+  @IsDate()
+  updated_at: Date
 }
