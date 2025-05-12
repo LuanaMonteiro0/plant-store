5b94ca736ec2f8631df9f78d4909159448cb4656
lumonteiro0 <luxo11111@gmail.com>
2025-05-11 21:14:53 -0300

    feat: plant type controller quase pronto

--- apps/plant-store/src/modules/plant-type/plant-type.controller.ts
+++ apps/plant-store/src/modules/plant-type/plant-type.controller.ts
@@ -1,7 +1,37 @@
 import { TunnelCatFactory } from '@core/tunnelCat/tunnel-cat.factory'
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param } from '@nestjs/common'
+import { getPlantType, GetPlantTypeDTO } from './use-cases/get-plant-type'
 
 @Controller()
 export class PlantTypeController {
   constructor(private readonly tunnelCat: TunnelCatFactory) {}
+
+    @Get('/')
+    async listPlants() {
+      const client = await this.tunnelCat.connect()
+  
+      try {
+        //const response = await listPlantTypes(client)
+  
+        return response
+      } catch (err) {
+        throw err
+      } finally {
+      }
+    }
+  
+    @Get('/:id')
+    async getPlant(@Param() param: GetPlantTypeDTO) {
+      const client = await this.tunnelCat.connect()
+  
+      try {
+        const response = await getPlantType(client, {...param})
+  
+        return response
+      } catch (err) {
+        throw err
+      } finally {
+        client.release()
+      }
+    }
 }
